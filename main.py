f069ee63f40f8d2bc8c4dbdf6570a591756dc0b3
dragonxeno195 <80433867+dragonxeno195@users.noreply.github.com>
2024-05-28 18:47:49 +0000

    Added a lives counter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-#Step 1 
+#Step 1
 
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -7,14 +7,23 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
-
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+lifes = 6
+num_of_letters = 0
+
 for letter in chosen_word:
-  if letter == guess:
-    print("Right")
-  else:
-    print("Wrong")
+    if letter == guess:
+        print(f"The letter {guess} is in the word")
+        num_of_letters += 1
+    else:
+        print("Wrong")
+
+if num_of_letters == 0:
+    lifes -= 1
+    print(f"You have {lifes} lifes left")
+
+print(f"You have {lifes} lifes left")
