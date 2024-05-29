cea124224fc3bde73cfd4d4438fbbe8489b14c66
dragonxeno195 <80433867+dragonxeno195@users.noreply.github.com>
2024-05-29 10:22:45 -0400

    Update main.py
    Added Display

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import random
-#Step 1
-
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 chosen_word = random.choice(word_list)
-
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 
 guess = input("Guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 lifes = 6
 num_of_letters = 0
 
-for letter in chosen_word:
+display = []
+for _ in chosen_word:
+    display.append("_")
+    
+for postion in range(len(chosen_word)):
+    letter = chosen_word[postion]
     if letter == guess:
-        print(f"The letter {guess} is in the word")
-        num_of_letters += 1
+        display[postion] = letter
     else:
         print("Wrong")
 
